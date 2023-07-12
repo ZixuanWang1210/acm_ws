a105258af29605a408f176df354311851a060fdf
WangZixuan <38860063+ZixuanWang1210@users.noreply.github.com>
2023-07-12 14:52:07 +0800

    Update ConvexHull2D.cpp

--- template/ConvexHull2D.cpp
+++ template/ConvexHull2D.cpp
@@ -1,33 +1,3 @@
-PDD p[N];
-int stk[N], top;
-bool used[N];
-//used数组表示这个点是否已经在底边上了，如果在了，就不用考虑把他放在顶边上了。
-//注意在我们在求顶的时候，要把used[0]变成false
-//因为我们求的闭包长度 【首是0 末是0 闭环】
-
-//重载加减  叉积
-PDD operator+(PDD a,PDD b){
-    return {a.x+b.x,a.y+b.y};
-}
-PDD operator-(PDD a,PDD b){
-    return {a.x - b.x,a.y-b.y};
-}
-double operator*(PDD a,PDD b){
-    return a.x*b.y - a.y*b.x;
-}
-
-//判断顺时针还是逆时针 有向面积
-double area(PDD a, PDD b, PDD c) {
-    return (b - a) * (c - a);
-}
-
-double dis(PDD a, PDD b) {
-    double dx = a.x - b.x;
-    double dy = a.y - b.y;
-    return sqrt(dx * dx + dy * dy);
-}
-
-
 //Andrew算法
 double getConvex() {
     // pair 默认对first升序，当first相同时对second升序；
